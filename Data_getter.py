b38b28b9a6953af60a06c4f1797caf401f414b70
Jozuaheule <118452285+Jozuaheule@users.noreply.github.com>
2023-01-11 21:27:36 +0100

    Data getter

--- Data_getter.py
+++ Data_getter.py
@@ -1,21 +1,23 @@
 import numpy as np
 
-with open("Supersonic Mach-Area.txt", "r") as f:
-    lines = f.readlines()
-    print(len(lines))
+def get_data_area():
 
-    h = []
-    x = []
-    area_relation = []
+    with open("Supersonic Mach-Area.txt", "r") as f:
+        lines = f.readlines()
+        print(len(lines))
 
-    for elem in lines:
-        data = elem.split(" ")
-        h.append(float(data[0]))
-        x.append(float(data[1]))
-        area_relation.append((float(data[2])))
+        h = []
+        x = []
+        area_relation = []
 
-    h = np.array(h)
-    x = np.array(x)
-    area_relation = np.array(area_relation)
+        for elem in lines:
+            data = elem.split(" ")
+            h.append(float(data[0]))
+            x.append(float(data[1]))
+            area_relation.append((float(data[2])))
 
-print(h)
+        h = np.array(h)
+        x = np.array(x)
+        area_relation = np.array(area_relation)
+
+    return h, x, area_relation
