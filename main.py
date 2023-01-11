b2042c5e0f193172c3937da5b6bc522d5a26843c
JorisvEijk <81755197+JorisvEijk@users.noreply.github.com>
2023-01-11 22:44:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,35 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+import matplotlib.pyplot as plt
+import Data_getter
+import flowtools
 
+xThroat = 65.0 #mm
+gamma = 1.4
+data = Data_getter.get_data_area()
+xvals = data[0]
+heights = data[1]
+areas = data[2]
+print(data)
+pressureRatios = np.array([])
+MachNums = np.array([])
+for i in range(len(xvals)):
+    A = areas[i]
+    if xvals[i] < xThroat:
+        mode = 'sub'
+    else:
+        mode = 'sup'
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    results = flowtools.flowisentropic2(gamma, A, mode)
+    MachNums = np.append(MachNums, results[0])
+    pressureRatios = np.append(pressureRatios, results[2])
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+print(pressureRatios)
+print(MachNums)
+plt.plot(xvals, MachNums)
+plt.plot(xvals, pressureRatios)
+plt.show()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
