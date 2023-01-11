aac2eb31d5c97ffd9e1f607fdde15ec5f5807e83
Jozuaheule <118452285+Jozuaheule@users.noreply.github.com>
2023-01-11 21:26:42 +0100

    Data getter

--- Data_getter.py
+++ Data_getter.py
@@ -1,4 +1,21 @@
+import numpy as np
+
 with open("Supersonic Mach-Area.txt", "r") as f:
     lines = f.readlines()
+    print(len(lines))
 
-print(lines)
+    h = []
+    x = []
+    area_relation = []
+
+    for elem in lines:
+        data = elem.split(" ")
+        h.append(float(data[0]))
+        x.append(float(data[1]))
+        area_relation.append((float(data[2])))
+
+    h = np.array(h)
+    x = np.array(x)
+    area_relation = np.array(area_relation)
+
+print(h)
